9e2b6bc402a4df97e9899695c997db19998ea0c1
dujichong <dujichong@finupgroup.com>
2019-06-05 14:22:19 +0800

    增加滚动高度,下拉刷新

--- _posts/my-practise.js
+++ _posts/my-practise.js
@@ -87,34 +87,70 @@ axios.post( '/user/12345', {
 source.cancel( 'Operation canceled by the user.' );
 
 // 单例模式
-var getInstance = function(fn){
+var getInstance = function ( fn ) {
   let result;
-  return result || (result = fn.call(this, arguments));
+  return result || ( result = fn.call( this, arguments ) );
 }
 
 // 防抖和节流都用闭包
-function debounce(fn, interval = 300){
+function debounce ( fn, interval = 300 ) {
   let timeout = null;
-  return function(...args){
-    clearTimeout(timeout);
-    setTimeout(() => {
-      fn.apply(this, args)
-    }, interval)
+  return function ( ...args ) {
+    clearTimeout( timeout );
+    setTimeout( () => {
+      fn.apply( this, args )
+    }, interval )
   }
 }
 
-function throttle(fn, interval){
+function throttle ( fn, interval ) {
   let canRun = true;
-  return function(...args){
-    if(!canRun) return;
+  return function ( ...args ) {
+    if ( !canRun ) return;
     canRun = false;
-    setTimeout(() => {
-      fn.apply(this, args);
+    setTimeout( () => {
+      fn.apply( this, args );
       canRun = true;
-    }, interval)
+    }, interval )
 
   }
 }
 
 
 
+// 下拉刷新
+( function ( window ) {
+  var _element = document.getElementById( 'refreshContainer' ),
+    _refreshText = document.querySelector( '.refreshText' ),
+    _startPos = 0,
+    _transitionHeight = 0;
+
+  _element.addEventListener( 'touchstart', function ( e ) {
+    console.log( '初始位置：', e.touches[ 0 ].pageY );
+    _startPos = e.touches[ 0 ].pageY;
+    _element.style.position = 'relative';
+    _element.style.transition = 'transform 0s';
+  }, false );
+
+  _element.addEventListener( 'touchmove', function ( e ) {
+    console.log( '当前位置：', e.touches[ 0 ].pageY );
+    _transitionHeight = e.touches[ 0 ].pageY - _startPos;
+
+    if ( _transitionHeight > 0 && _transitionHeight < 60 ) {
+      _refreshText.innerText = '下拉刷新';
+      _element.style.transform = 'translateY(' + _transitionHeight + 'px)';
+
+      if ( _transitionHeight > 55 ) {
+        _refreshText.innerText = '释放更新';
+      }
+    }
+  }, false );
+
+  _element.addEventListener( 'touchend', function ( e ) {
+    _element.style.transition = 'transform 0.5s ease 1s';
+    _element.style.transform = 'translateY(0px)';
+    _refreshText.innerText = '更新中...';
+
+    // todo...
+  }, false );
+} )( window );
